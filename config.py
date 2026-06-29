b4bea313b7ca4cbf6002a7dc3d17ecfd7bc41b6c
rishabhp456 <patilrisshabh22@gmail.com>
2026-06-29 20:23:03 +0530

    port changed for azure deploy

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ user_collection_name = "collection_user"
 data_collection_name = "collection_data"
 
 FLASK_HOST = "0.0.0.0"
-FLASK_PORT = 5005
+FLASK_PORT = 8000
 
 ML_MODEL_PATH = os.path.join(os.getcwd(), "artifacts", "LR_med_ins.pkl")
 INPUT_DATA_PATH = os.path.join(os.getcwd(), "data", "medical_insurance.csv")
